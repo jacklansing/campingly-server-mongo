3e43a1d363944b70fd87fb551a33f87b9f9c6b74
Jack Lansing <jack.lansing@live.com>
2020-12-10 10:02:09 -0500

    refactor: add doc to useValidationSchema

--- src/utils/validators/useValidationSchema.ts
+++ src/utils/validators/useValidationSchema.ts
@@ -2,6 +2,11 @@ import { ValidationError } from 'apollo-server-express';
 import { FieldError } from '../../resolvers/types/shared.types';
 import { ObjectSchema } from 'yup';
 
+/**
+ * Compares a set of given values against the provided yup schema. Returns errors in a consistent format as a field error array.
+ * @param values The values to compare against the schema.
+ * @param Schema The Object Schema from yup
+ */
 export const useValidationSchema = async (
   values: object,
   Schema: ObjectSchema,
